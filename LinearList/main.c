0007c7af385f5a46e988634333f6287568abd8e5
iiiCode <iiiCode@aliyun.com>
2015-04-22 11:57:38 +0800

    Add Test samples for Linear list.
    Signed-off-by: iiiCode <iiiCode@aliyun.com>

---
+++ LinearList/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "LinearList.h"
+
+#define SIZE 20
+
+static void array_init(int *arr, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i ++) {
+        arr[i] = rand() % 90 + 10;
+    }
+}
+
+static void array_show(int *arr, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i ++) {
+        printf("%d ", arr[i]);
+    }
+
+    putchar('\n');
+}
+
+static void traverse(void *data)
+{
+    printf("%d ", *((int *)data));
+}
+
+static void destroy(void *data)
+{
+    free(data);
+}
+
+void data_traverse(LinearList *list)
+{
+    int i;
+    int *v;
+
+    for (i = 0; i < LinearListLength(list); i ++) {
+        LinearListGetElement(list, i, (void **)&v);
+        printf("%d ", *v);
+    }
+
+    putchar('\n');
+}
+
+int main(int argc, char *argv[])
+{
+    int pos;
+    int i, *v;
+    int arr[SIZE];
+    LinearList list;
+
+    //srand(time(NULL));
+
+    LinearListInit(&list);
+    array_init(arr, SIZE);
+    array_show(arr, SIZE);
+
+    for (i = 0; i < SIZE; i ++) {
+        v = malloc(sizeof(int));
+        *v = arr[i];
+        LinearListInsert(&list, 0, v);
+    }
+
+    LinearListTraverse(&list, traverse); putchar('\n');
+
+    LinearListClear(&list, destroy);
+
+    for (i = 0; i < SIZE; i ++) {
+        v = malloc(sizeof(int));
+        *v = arr[i];
+        LinearListInsert(&list, LinearListLength(&list), v);
+    }
+
+    LinearListTraverse(&list, traverse); putchar('\n');
+
+    while(! LinearListEmpty(&list)) {
+        pos = rand() % LinearListLength(&list);
+        LinearListGetElement(&list, pos, (void **)&v);
+        printf("Delete: %d\n", *v);
+        LinearListDelete(&list, pos, (void **)&v);
+        data_traverse(&list);
+    }
+
+    LinearListDestroy(&list, destroy);
+
+    return 0;
+}
+
