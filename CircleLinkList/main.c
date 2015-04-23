4679e1b10ad6c654aba68f4c035252d358050e5a
iiiCode <iiiCode@aliyun.com>
2015-04-23 10:30:55 +0800

    Add Test case for Circle link list.
    Signed-off-by: iiiCode <iiiCode@aliyun.com>

--- CircleLinkList/main.c
+++ CircleLinkList/main.c
@@ -5,6 +5,11 @@
 #include "utils.h"
 #include "CircleLinkList.h"
 
+static int compare(const void *first, const void *second)
+{
+    return *(int *)first - *(int *)second;
+}
+
 static void traverse(const void *data)
 {
     printf("%d ", *(int *)data);
@@ -36,6 +41,21 @@ int main(int argc, char *argv[])
     putchar('\n');
     CircleLinkListTraverse(&list, traverse);
     putchar('\n');
+
+    while(! CircleLinkListEmpty(&list)) {
+        i = rand() % 90 + 10;
+        
+        const int *v = CircleLinkListSearch(&list, &i, compare);
+
+        if (v) {
+            printf("Delete: %d\n", *v);
+            CircleLinkListDelete(&list, v, compare);
+            CircleLinkListTraverse(&list, traverse);
+            putchar('\n');
+            int *vv = (int *)v;
+            FREE(vv);
+        }
+    }
 
     CircleLinkListDestroy(&list, destroy);
     return 0;
