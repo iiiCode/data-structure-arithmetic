9496eb9b9be2ed676a02223808c584e1b860d84a
iiiCode <iiiCode@aliyun.com>
2015-04-24 10:13:04 +0800

    Complete DuLinkList.
    Signed-off-by: iiiCode <iiiCode@aliyun.com>

--- DuLinkList/DuLinkList.h
+++ DuLinkList/DuLinkList.h
@@ -1,8 +1,10 @@
 #ifndef _DOUBLE_DIRECTION_LINK_LIST_H_
 #define _DOUBLE_DIRECTION_LINK_LIST_H_
 
+#include <stdbool.h>
+
 typedef struct node {
-    const void *data;
+    void *data;
     struct node *prev;
     struct node *next;
 } node;
@@ -10,5 +12,13 @@ typedef struct node {
 typedef struct DuLinkList {
     struct node *head;
 } DuLinkList;
+
+extern void DuLinkListInit(DuLinkList *list);
+extern bool DuLinkListEmpty(DuLinkList *list);
+extern void DuLinkListInsert(DuLinkList *list, void *data);
+extern void DuLinkListDestroy(DuLinkList *list, void (*destroy)(void *));
+extern void DuLinkListTraverse(DuLinkList *list, void (*traverse)(void *));
+extern void * DuLinkListSearch(DuLinkList *list, void *key, int (*compare)(const void *, const void *));
+extern void * DuLinkListDelete(DuLinkList *list, void *key, int (*compare)(const void *, const void *));
 
 #endif //_DOUBLE_DIRECTION_LINK_LIST_H_
